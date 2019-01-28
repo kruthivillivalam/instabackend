54ecd18749c18665459e3b93e95a215749ecc435
Kruthi Villivalam <krvillia@microsoft.com>
2019-01-28 10:27:32 +0530

    init

--- routes/getusers.js
+++ routes/getusers.js
@@ -13,8 +13,40 @@ const url ="mongodb://kruthi:Hope1993@ds255930.mlab.com:55930/userslist";
             db = client.db("userslist");
         }
     })
-router.get('/',function(req,res){
-    console.log("DB"+db)
+router.post('/',function(req,res){
+    console.log("Res"+req.headers.user)
+    console.log("Res"+req.headers.pwd)
+    const collection = db.collection('getusers')
+    let p = new Promise((resolve,reject) => {
+        collection.find({'username':req.headers.user}).toArray(function(err,docs){
+            console.log("docs"+JSON.stringify(docs))
+            if(docs.length === 0){
+                console.log("No user exists")
+                resolve(
+                    [{
+                        res: "Hello",
+                        user:req.headers.user,
+                        pwd:req.headers.pwd
+                    }]
+                ) 
+            }
+            else{
+                console.log("User already exists")
+                reject("User already exists")
+            }
+            
+        })
+    })
+    p.then(response => {
+       console.log("Docs===="+response)
+       res.send(response)
+
+    },err => {
+        console.log("Error===="+err)
+    })
+    
+   
+    //console.log("DB"+db)
 });
 
 module.exports = router;
